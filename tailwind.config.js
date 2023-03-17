822ac1a9c0c077711e684a4a0b2261bd1463d4f9
Son Ngoc Nguyen <74242957+JensonNg@users.noreply.github.com>
2023-03-17 16:09:37 +0100

    fix bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['dist/**/*.html'],
+  purge: ['scss/**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
